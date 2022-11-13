ec55e9efc0c07754159809d7f134765d5ad7c282
Programacion <programacion@brandgroup.com.py>
2022-11-13 19:48:36 -0300

    se agregaron mejoras a algunas tablas

--- create table STK_MOVILES.sql
+++ create table STK_MOVILES.sql
@@ -6,10 +6,18 @@ create table STK_MOVILES
   mov_abrev  VARCHAR2(20) not null,
   mov_marca  VARCHAR2(50) not null,
   mov_chapa  VARCHAR2(50) not null,
-  mov_año    VARCHAR2(5) not null,
+  mov_anho_fab VARCHAR2(5) not null,
   mov_estado NUMBER not null
 )
 
 -- Create/Recreate primary, unique and foreign key constraints 
 alter table STK_MOVILES
   add constraint STK_MOVILES_PK primary key (MOV_CLAVE);
+
+
+/
+-- Add/modify columns 
+alter table STK_MOVILES add mov_fech_adquisicion date;
+-- Add comments to the columns 
+comment on column STK_MOVILES.mov_fech_adquisicion
+  is 'fecha en la institucion adquirio el movil';
